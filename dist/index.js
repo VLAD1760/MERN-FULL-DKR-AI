aeb0a0d8afb060b9db2440138050175b2ad07357
VLAD170 <vladdd760@gmail.com>
2024-03-17 18:51:28 -0600

    backend mongo setup & ready

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,9 @@
-import express from 'express';
-const app = express();
-app.listen(5000, () => console.log("Server Open"));
+import app from "./app.js";
+import { connectToDatabase } from "./db/connection.js";
+const PORT = process.env.PORT || 5000;
+connectToDatabase()
+    .then(() => {
+    app.listen(PORT, () => console.log("Server Open & Connected to Database"));
+})
+    .catch((err) => console.log(err));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
-import express from  'express'
+import app from "./app.js"
+import { connectToDatabase } from "./db/connection.js";
 
-const app = express();
-
-app.listen(5000, () => console.log("Server Open"));
+const PORT = process.env.PORT || 5000;
+connectToDatabase()
+  .then(() => {
+    app.listen(PORT, () => 
+      console.log("Server Open & Connected to Database")
+    );
+  })
+  .catch((err) => console.log(err));
+  
